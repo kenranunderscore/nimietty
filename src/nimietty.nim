96e18b47524b66030b6e19afc938fb8d5e43c94a
Johannes Maier <johb.maier@gmail.com>
2020-12-10 15:21:26 +0100

    Communicate end of main via UNIX pipe

--- src/nimietty.nim
+++ src/nimietty.nim
@@ -1,6 +1,5 @@
 import sdl2
 from pty import nil
-from os import sleep
 from posix import nil
 
 type
@@ -9,24 +8,36 @@ type
   TerminalState = object
     grid: array[10, array[80, char]]
     pos: CursorPosition
+  ReaderFds = object
+    pty: cint
+    commPipe: cint
 
 var
   state = TerminalState(pos: CursorPosition(x: 0, y: 0))
 
-proc readPtyAndUpdateState(fd: cint) =
+proc readPtyAndUpdateState(fds: ReaderFds) =
   var wrapped = false
-  let nFd: cint = fd + 1
-  var fds: posix.TFdSet
-  posix.FD_ZERO(fds)
-  posix.FD_SET(fd, fds)
+  let nFd: cint = max(fds.pty, fds.commPipe) + 1
+  var readable: posix.TFdSet
   # TODO stop using Timeval of 0 to make select block again, as it should
   var tv: posix.Timeval
   # TODO how to find a good value here? `BUFSIZ` maybe?
   const bufferSize = 512
-  while posix.select(nFd, addr(fds), nil, nil, addr(tv)) > 0:
-    if posix.FD_ISSET(fd, fds) > 0:
+  while true:
+    posix.FD_ZERO(readable)
+    posix.FD_SET(fds.commPipe, readable)
+    posix.FD_SET(fds.pty, readable)
+    if posix.select(nFd, addr(readable), nil, nil, nil) <= 0:
+      echo "select() failed; quitting"
+      quit(1)
+    echo "selected"
+    if posix.FD_ISSET(fds.commPipe, readable) > 0:
+      echo "read from pipe"
+      # TODO actually read something? or use something else, maybe semaphore?
+      break
+    if posix.FD_ISSET(fds.pty, readable) > 0:
       var buf: array[bufferSize, uint8]
-      let bytesRead = posix.read(fd, addr(buf), bufferSize)
+      let bytesRead = posix.read(fds.pty, addr(buf), bufferSize)
       if bytesRead == 0:
         echo "Nothing to read, quitting"
         quit(0)
@@ -67,6 +78,21 @@ when isMainModule:
     var evt = sdl2.defaultEvent
     var running = true
     var counter = 0
+    # Run PTY reader and state update in a background thread.
+    # We open a UNIX pipe to communicate the end of the main program
+    # with the background/reader thread.
+    var pipeFds: array[2, cint]
+    if posix.pipe(pipeFds) < 0:
+      # TODO errno
+      echo "couldn't open pipe"
+      quit(1)
+    defer:
+      discard posix.close(pipeFds[0])
+      discard posix.close(pipeFds[1])
+    var reader: Thread[ReaderFds]
+    createThread(reader, readPtyAndUpdateState, ReaderFds(pty: tty.masterFd, commPipe: pipeFds[0]))
+    defer: joinThread(reader)
+
     while running:
       counter += 1
       while sdl2.pollEvent(evt):
@@ -74,8 +100,6 @@ when isMainModule:
           echo "quitting..."
           running = false
           break
-
-      readPtyAndUpdateState(tty.masterFd)
 
       # Write some dummy stuff to the PTY
       if counter == 200:
@@ -89,6 +113,9 @@ when isMainModule:
       renderer.clear()
       renderer.present()
 
+    # FIXME
+    var foo = "foo"
+    discard posix.write(pipeFds[1], addr(foo), 4)
     echo "final state: ", state.grid[0..2]
     destroy renderer
     destroy window
